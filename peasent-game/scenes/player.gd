bd12fc764f816b46e147c321f17a9fe67655282a
Scrambled <smartsnakie15@gmail.com>
2025-02-27 19:10:05 -0500

    notes

--- peasent-game/scenes/player.gd
+++ peasent-game/scenes/player.gd
@@ -1,5 +1,6 @@
 extends Area2D
-
+##add velocity logic and stuff
+#make custom key mapping in project settings
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
