dbf56d423671616aae321adecbc0631510d698e3
Scrambled <smartsnakie15@gmail.com>
2025-02-28 23:15:54 -0500

    erm player doesnt spawn

--- peasent-game/scenes/player.gd
+++ peasent-game/scenes/player.gd
@@ -18,7 +18,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	#jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		print("hi")
 	
+	#input direction obtaining
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
 		
