5c713e7b8e98c6b81a0ca15007819a3e3b924b6d
Scrambled <smartsnakie15@gmail.com>
2025-03-01 23:12:18 -0500

    button logic

--- peasent-game/scenes/travel.gd
+++ peasent-game/scenes/travel.gd
@@ -45,12 +45,55 @@ func _on_player_hazard_hit() -> void:
 	
 
 func _on_choice_window_choice_1() -> void:
+	#button hide
+	$choiceWindow/choice2.hide()
+	#gen random number
 	var num = randi_range(1, 5)
+	
+	#subtract from gold amt bc bandits
 	globals.gold -= num
+	
+	#logic to see if its game over
+	if globals.gold == 0:
+		$HUD.update_goldCounter(globals.gold)
+		$choiceWindow.updateHeader("you've lost all your gold...")
+	else:
+		#update values and continue game
+		$HUD.update_goldCounter(globals.gold)
+		var value = "you lost " + str(num) + " gold..."
+		$choiceWindow.updateHeader(value) 
+
+func _on_choice_window_choice_2() -> void:
+	#hide button
+	$choiceWindow/choice1.hide()
+	var num1 = randi_range(1, 5)
+	var num2 = randi_range(1,3)
+	globals.gold -= num1
+	globals.food -= num2
+	
+	#this could probably be made into a function but im tired and dgaf rn
 	if globals.gold == 0:
 		$HUD.update_goldCounter(globals.gold)
 		$choiceWindow.updateHeader("you've lost all your gold...")
 	else:
 		$HUD.update_goldCounter(globals.gold)
-		var value = "you lost" + str(num) + "gold..."
-		$choiceWindow.updateHeader(value) 
+		
+	if globals.food == 0:
+		$choiceWindow.updateHeader("you've lost all your food and cannot continue...")
+		$HUD.update_foodCounter(globals.food)
+	else:
+		$HUD.update_goldCounter(globals.gold)
+		$HUD.update_foodCounter(globals.food)
+		var value = "you lost " + str(num1) + " gold and " + str(num2) + " food escaping"
+		$choiceWindow.updateHeader(value)
+	
+
+
+func _on_choice_window_choice_made() -> void:
+	pass # Replace with function body.
+	#delete hazard child
+	#restart hazard timer
+	#hide choice window
+	
+#create function to check if food is completely depleted
+#if yes, then end the game
