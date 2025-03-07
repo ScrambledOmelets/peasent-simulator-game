87f4210d1e1a8ce489dde65a780f0f703786a0bd
Scrambled <smartsnakie15@gmail.com>
2025-03-07 13:18:49 -0500

    update player tilemap background color

---
+++ 2d-peasent-rpg/scripts/main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+#homescreen is set to the main scene
+#need to set up to change scenes with get_tree().change_scene_to_file() or something
+	#scene change from homescreen to the game scene
+	#then change back again at the end
+#main file will handle backend code so it cant be main scene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
