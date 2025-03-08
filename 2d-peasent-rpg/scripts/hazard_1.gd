3bf9d9f0d334ffc8a9a319fb3656fcfb4248f0f3
Scrambled <smartsnakie15@gmail.com>
2025-03-07 20:52:19 -0500

    timers and harzard signal emit

--- 2d-peasent-rpg/scripts/hazard_1.gd
+++ 2d-peasent-rpg/scripts/hazard_1.gd
@@ -1,1 +1,9 @@
 extends Area2D
+signal playerHit
+
+func _ready() -> void:
+	$AnimatedSprite2D.play("default")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		playerHit.emit()
