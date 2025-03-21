551049cac32fec59c76e7c69eb867d9d6e5e728a
Scrambled <smartsnakie15@gmail.com>
2025-03-21 00:00:09 -0400

    updating bandit hazard

--- 2d-peasent-rpg/scripts/hazard_2.gd
+++ 2d-peasent-rpg/scripts/hazard_2.gd
@@ -2,15 +2,19 @@ extends Area2D
 #signal playerHit
 @export var speed = 500
 
+var playerPresent = false
 
 func _ready() -> void:
-	$AnimatedSprite2D.play("coin")
+	$AnimatedSprite2D.play("standing")
 	
 
 func _process(delta: float) -> void:
 	#wtf this is so simple and it makes the object follow the player
-	var direction = (SignalBus.player_location - global_position).normalized()
-	position += speed * direction * delta 
+	if playerPresent == true:
+		var direction = (SignalBus.player_location - global_position).normalized()
+		position += speed * direction * delta 
+	else:
+		pass
 	
 
 #signal works too
@@ -20,3 +24,11 @@ func _on_body_entered(body: Node2D) -> void:
 		print("they hit")
 		#disable movement
 		set_physics_process(false)
+
+
+func _on_player_in_range_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_player_in_range_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
