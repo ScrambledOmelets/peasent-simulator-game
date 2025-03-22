64ef1d8f2a905b6e7a7f76128385e29d0da03009
Scrambled <smartsnakie15@gmail.com>
2025-03-22 02:11:57 -0400

    debugging ferryman  hazard

--- 2d-peasent-rpg/scripts/ferry_man.gd
+++ 2d-peasent-rpg/scripts/ferry_man.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var inChat = false
+var inRange = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,17 +11,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept") and inChat == false:
+	if Input.is_action_just_pressed("ui_accept") and inChat == false and inRange == true:
 		$toolTip.hide()
 		DialogueManager.show_dialogue_balloon(load("res://scripts/dialogue.dialogue"), "ferry_man")
 		inChat = true
-		
 		
 func _on_dialouge_ended(resource: DialogueResource):
 	inChat = false
 
 func _on_conversation_range_body_entered(body: Node2D) -> void:
 	$toolTip.show()
+	inRange  = true
+	
 
 func _on_conversation_range_body_exited(body: Node2D) -> void:
 	$toolTip.hide()
+	inRange = false
