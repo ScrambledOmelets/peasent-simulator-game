44ae2869a572ab095b0bdca3897bd3e63276884b
Scrambled <smartsnakie15@gmail.com>
2025-03-22 02:04:09 -0400

    ferryman dialouge and char sizing

--- 2d-peasent-rpg/scripts/ferry_man.gd
+++ 2d-peasent-rpg/scripts/ferry_man.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
 
+var inChat = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$toolTip.hide()
-
+	DialogueManager.dialogue_ended.connect(_on_dialouge_ended)
+	$AnimatedSprite2D.play("standing")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	if Input.is_action_just_pressed("ui_accept") and inChat == false:
+		$toolTip.hide()
+		DialogueManager.show_dialogue_balloon(load("res://scripts/dialogue.dialogue"), "ferry_man")
+		inChat = true
+		
+		
+func _on_dialouge_ended(resource: DialogueResource):
+	inChat = false
 
 func _on_conversation_range_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
+	$toolTip.show()
 
 func _on_conversation_range_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	$toolTip.hide()
